5ba70424ea67dc5ffe2b1c6a00043305dd4c6a5a
Alejandro-R-Reche <arodreche@gmail.com>
2025-05-20 16:02:58 -0300

    [INIT] Created .gitignore file. [INIT] Created first minimal API TodoApi for first single microservice.

---
+++ TodoApi/TodoApi/TodoDB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi
+{
+    public class TodoDB : DbContext
+
+    {
+        public TodoDB(DbContextOptions<TodoDB> options)
+            : base(options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
