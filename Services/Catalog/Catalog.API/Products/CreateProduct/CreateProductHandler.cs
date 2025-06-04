3ff27ff37f0d6de1f62b8e1d139fda4ef98122da
Alejandro-R-Reche <arodreche@gmail.com>
2025-06-04 15:16:46 -0300

    [REFACTOR] Changed project structure for first microservice. [WIP] Working on first endpoints for CatalogAPI microservice.

---
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product:
+        //
+        // 1) Create Product entity from command object.
+        // 2) Save to database.
+        // 3) Return CreateProductResult result.
+
+        // 1) Create Product entity from command object.
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            Price = command.Price
+        };
+
+        // 2) Save to database (we skip for now).
+        // 3) Return CreateProductResult result.
+        return new CreateProductResult(Id: Guid.NewGuid());
+    }
+}
